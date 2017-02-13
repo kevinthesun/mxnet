9a988db5b523e26859968b9997e2938e98f7b40c
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-02-13 12:46:19 -0800

    Add running script

--- tests/nightly/test_notebook.sh
+++ tests/nightly/test_notebook.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 echo "BUILD make"
 cp ./make/config.mk .
-echo "USE_CUDA=0" >> ./config.mk
-echo "USE_CUDNN=0" >> ./config.mk
-echo "USE_BLAS=openblas" >> ./config.mk
-echo "ADD_CFLAGS += -I/usr/include/openblas" >> ./config.mk
-echo "GTEST_PATH=/usr/local/gtest" >> ./config.mk
-echo 'export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH' >> ~/.profile
-echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.profile
-echo 'export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.111-1.b15.25.amzn1.x86_64' >> ~/.profile
-echo 'export JRE_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.111-1.b15.25.amzn1.x86_64/jre' >> ~/.profile
-echo 'export PATH=$PATH:/apache-maven-3.3.9/bin/:/usr/bin:/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.111-1.b15.25.amzn1.x86_64/bin:/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.111-1.b15.25.amzn1.x86_64/jre/bin' >> ~/.profile
-source ~/.profile
+echo "USE_CUDA=1" >> ./config.mk
+echo "USE_CUDA_PATH=/usr/local/cuda" >> ./config.mk
+echo "USE_CUDNN=1" >> ./config.mk
+echo "USE_PROFILER=1" >> ./config.mk
+echo "DEV=1" >> ./config.mk
+echo "EXTRA_OPERATORS=example/ssd/operator" >> ./config.mk
 make clean
-make -j 4 || exit -1
+make -j$(nproc) || exit -1
 
 echo "BUILD python2 mxnet"
 cd ./python
