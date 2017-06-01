eb62b390551bdff2d2ce7501d65b98cec7606836
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-05-31 18:10:56 -0700

    Refactor

--- tests/nightly/test_tutorial.sh
+++ tests/nightly/test_tutorial.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#Setup virtualenv and install packages
+virtualenv ENV
+source /ENV/bin/activate
+
+pip install --upgrade setuptools
+pip install requests
+pip install jupyter
+pip install graphviz
+pip install matplotlib
+
 #Test tutorials
 make docs
 cd tests/nightly
