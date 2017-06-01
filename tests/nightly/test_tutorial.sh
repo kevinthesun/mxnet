91cf20f1eb05493392a2a74b7ad856f9feb30bcd
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-05-31 23:46:22 -0700

    Fix

--- tests/nightly/test_tutorial.sh
+++ tests/nightly/test_tutorial.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-#Build mxnet and docs
-#cp make/config.mk .
-#make -j8 USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1 || exit 1
-#cd python
-#sudo python setup.py install
-cd docs
+Build mxnet and docs
+cp make/config.mk .
+make -j8 USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1 || exit 1
+cd python
+sudo python setup.py install
+cd ../docs
 make html
 
 #Setup virtualenv and install packages
+cd ../tests/nightly
 virtualenv ENV
-source /workspace/docs/ENV/bin/activate
+source /workspace/tests/nightly/ENV/bin/activate
 pip install six
 
 pip install requests
 pip install jupyter
 pip install graphviz
 pip install matplotlib
-
-cd ../python
-sudo python setup.py install
+sudo python ../../python/setup.py install
 
 #Test tutorials
-cd ../tests/nightly
 python test_tutorial.py
