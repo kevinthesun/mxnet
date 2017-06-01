88a9059b535a614867405f458177e47e8694737d
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-06-01 15:03:45 -0700

    Fix

--- tests/nightly/test_tutorial.sh
+++ tests/nightly/test_tutorial.sh
@@ -5,17 +5,24 @@ make -j8 USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1 || exit 1
 cd docs
 make html
 
-#Setup virtualenv and install packages
 cd ../python
-virtualenv ENV
-source /workspace/python/ENV/bin/activate
-pip install six
-/workspace/python/ENV/bin/python setup.py install
+sudo python setup.py install
+sudo pip install requests
+sudo pip install jupyter
+sudo pip install graphviz
+sudo pip install matplotlib
 
-pip install requests
-pip install jupyter
-pip install graphviz
-pip install matplotlib
+#Setup virtualenv and install packages
+#cd ../python
+#virtualenv ENV
+#source /workspace/python/ENV/bin/activate
+#pip install six
+#/workspace/python/ENV/bin/python setup.py install
+
+#pip install requests
+#pip install jupyter
+#pip install graphviz
+#pip install matplotlib
 
 #Test tutorials
 cd ../tests/nightly
