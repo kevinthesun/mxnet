9c87cf3136204d370e220025b5d7c2c90a6a3a23
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-05-31 18:44:52 -0700

    Build mxnet from source

--- tests/nightly/test_tutorial.sh
+++ tests/nightly/test_tutorial.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+#Build mxnet and docs
+cp make/config.mk .
+make -j8 USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1 || exit 1
+cd python
+sudo python setup.py install
+cd ../docs
+make html
 
 #Setup virtualenv and install packages
 virtualenv ENV
 source /workspace/ENV/bin/activate
 
-pip install --upgrade setuptools
 pip install requests
 pip install jupyter
 pip install graphviz
 pip install matplotlib
 
+cd ../python
+python setup.py install
+
 #Test tutorials
-sudo make docs
-cd tests/nightly
+cd ../tests/nightly
 python test_tutorial.py
