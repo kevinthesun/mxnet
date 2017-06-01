d9088aec5c3cd16ed91b8d79a9ef90e43c971ce8
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-05-31 17:36:43 -0700

    Move virtualenv package to docker

--- tests/nightly/test_tutorial.sh
+++ tests/nightly/test_tutorial.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-#Setup virtualenv and install pip package
-virtualenv ENV
-
-pip install --upgrade setuptools
-pip install numpy
-pip install request
-pip install jupyter
-pip install graphviz
-pip install matplotlib
-
 #Test tutorials
-cp make/config.mk .
-make -j8 USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1 || exit 1
-cd python
-python setup.py install
-cd ..
 make docs
+source ~/ENV/bin/activate
 python tests/nightly/test_tutorial.py
