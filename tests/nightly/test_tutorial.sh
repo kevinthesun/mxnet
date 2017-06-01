e1d9d802c33ddad5254d463f3083616408f6a127
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-05-31 18:33:25 -0700

    Small fix

--- tests/nightly/test_tutorial.sh
+++ tests/nightly/test_tutorial.sh
@@ -2,7 +2,7 @@
 
 #Setup virtualenv and install packages
 virtualenv ENV
-source workspace/ENV/bin/activate
+source /workspace/ENV/bin/activate
 
 pip install --upgrade setuptools
 pip install requests
@@ -11,6 +11,6 @@ pip install graphviz
 pip install matplotlib
 
 #Test tutorials
-make docs
+sudo make docs
 cd tests/nightly
 python test_tutorial.py
