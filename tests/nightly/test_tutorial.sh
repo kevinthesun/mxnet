5dfe81022b8c35006713a25a90f9f4077cac5fba
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-06-02 10:54:26 -0700

    Fix

--- tests/nightly/test_tutorial.sh
+++ tests/nightly/test_tutorial.sh
@@ -6,7 +6,6 @@ wget http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.deb && \
     sudo dpkg -i scala-2.11.8.deb && rm scala-2.11.8.deb
 
 sudo apt-get -y install git
-sudo apt-get -y install python-opencv
 sudo apt-get -y install ipython ipython-notebook
 sudo apt-get -y install graphviz
 sudo apt-get -y install doxygen
@@ -19,6 +18,8 @@ sudo pip install sphinx==1.5.1 CommonMark==0.5.4 breathe mock==1.0.1 recommonmar
 sudo pip install --upgrade requests
 
 #Build mxnet and docs
+git clone https://github.com/dmlc/mxnet.git --recursive
+cd mxnet
 cp make/config.mk .
 make -j8 USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1
 cd docs
@@ -27,9 +28,10 @@ make html
 #Setup virtualenv and install packages
 cd ../python
 virtualenv ENV
-source /home/ec2-user/workspace/NightlyTutorialUbuntu/python/ENV/bin/activate
+source /home/ec2-user/workspace/NightlyTutorialUbuntu/mxnet/python/ENV/bin/activate
+sudo apt-get -y install python-opencv
 pip install six
-/home/ec2-user/workspace/NightlyTutorialUbuntu/python/ENV/bin/python setup.py install
+/home/ec2-user/workspace/NightlyTutorialUbuntu/mxnet/python/ENV/bin/python setup.py install
 
 pip install requests
 pip install jupyter
