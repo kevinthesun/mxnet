58075831b26a9d654fe1b441affd0035f2aa2b62
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-07-31 13:51:16 -0700

    Fix

--- benchmark/VGG16.py
+++ benchmark/VGG16.py
@@ -1,18 +1,22 @@
-import mxnet as mx
 import random
+import argparse
+import time
+import numpy as np
+
+import mxnet as mx
 from mxnet.io import DataBatch, DataIter
 from mxnet import gluon
 from mxnet.gluon import nn
 from mxnet import autograd as ag
 from mxnet.gluon.model_zoo.vision.vgg import vgg16
-import numpy as np
-import argparse
 
 parser = argparse.ArgumentParser(description="Gluon VGG16 Benchmark",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--num_gpu', type=int, default='16',
                         help='Number of gpus used. 0 means using cpu')
-parser.add_argument('--image_shape', type=str, default='32,3,299,299',
+parser.add_argument('--batch_size', type=int, default='32',
+                        help='Batch size')
+parser.add_argument('--image_shape', type=str, default='3,299,299',
                         help='Input image shape')
 parser.add_argument('--num_classes', type=int, default='1000',
                         help='Number of image classes')
@@ -66,23 +70,42 @@ if __name__ == '__main__':
     num_gpu = args.num_gpu
     ctx = [mx.cpu(0)] if num_gpu == 0 else [mx.gpu(i) for i in range(num_gpu)]
 
+    batch_size = args.batch_size
     num_classes = args.num_classes
-    data_shape = args.image_shape
+    data_shape = tuple([batch_size] + [int(l) for l in args.image_shape.split(',')])
     max_iter = args.max_iter
     train_data = SyntheticDataIter(num_classes, data_shape, max_iter, dtype=np.float32)
 
     model = vgg16()
     model.collect_params().initialize(mx.init.Xavier(magnitude=2.24), ctx=ctx)
-    trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.1})
+    trainer = gluon.Trainer(model.collect_params(), 'sgd', {'learning_rate': 0.1})
+    softmax_loss = gluon.loss.SoftmaxCrossEntropyLoss()
 
     epoch = args.num_epoch
     metric = mx.metric.Accuracy()
+    start_time = time.time()
     for i in range(epoch):
         train_data.reset()
         for batch in train_data:
-            data = gluon.utils.split_and_load(batch.data[0], ctx_list=ctx, batch_axis=0)
-            label = gluon.utils.split_and_load(batch.label[0], ctx_list=ctx, batch_axis=0)
+            data_split = gluon.utils.split_and_load(batch.data[0], ctx_list=ctx, batch_axis=0)
+            label_split = gluon.utils.split_and_load(batch.label[0], ctx_list=ctx, batch_axis=0)
             outputs = []
+            with ag.record():
+                for data, label in zip(data_split, label_split):
+                    out = model(data)
+                    loss = softmax_loss(out, label)
+                    ag.backward([loss])
+                    outputs.append(out)
+            metric.update(label_split, outputs)
+            trainer.step(batch.data[0].shape[0])
+        name, acc  =metric.get()
+        metric.reset()
+        print('training acc at epoch %d: %s=%f' % (i, name, acc))
+    total_time = time.time() - start_time
+    print("Training time: %s" % (str(total_time)))
+    print("Training time per epoch: %s" % (str(total_time / epoch)))
+    print("Number of images process per second: %s" % (str(batch_size * max_iter * epoch / total_time)))
+
 
 
 
