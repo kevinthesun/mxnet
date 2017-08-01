28f9b46426d100d82c6d4c13deaae4df36986e97
Ubuntu <ubuntu@ip-172-31-22-99.us-west-2.compute.internal>
2017-08-01 21:07:57 +0000

    Build website

--- python/mxnet/gluon/nn/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-# pylint: disable=wildcard-import
-"""Neural network layers."""
-
-from .basic_layers import *
-
-from .conv_layers import *
--- python/mxnet/gluon/rnn/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-# pylint: disable=wildcard-import
-"""Recurrent neural network module."""
-
-from .rnn_cell import *
-
-from .rnn_layer import *
